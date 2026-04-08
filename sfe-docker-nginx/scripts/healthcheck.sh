43dbf1c88b68830660527a5f28c57b73be9c6b6d
BOURGUIG Abdennour <abdennour@bourguig.net>
2026-04-08 19:20:29 +0100

    Update healthcheck.sh
    Updated healthcheck script to log CPU usage of running services and changed log file path.

--- sfe-docker-nginx/scripts/healthcheck.sh
+++ sfe-docker-nginx/scripts/healthcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LOGFILE="logs/healthcheck.log"
+LOGFILE="/var/www/html/logs/healthcheck.log"
 DATE=$(date "+%Y-%m-%d %H:%M:%S")
 
 SERVICES=("nginx_web" "php_fpm" "mariadb_db")
@@ -8,13 +8,14 @@ SERVICES=("nginx_web" "php_fpm" "mariadb_db")
 echo "[$DATE] Vérification des services Docker" >> $LOGFILE
 
 for SERVICE in "${SERVICES[@]}"; do
-    STATUS=$(docker inspect --format='{{.State.Running}}' $SERVICE 2>/dev/null)
+    STATUS=$(docker inspect --format="{{.State.Running}}" $SERVICE 2>/dev/null)
 
     if [ "$STATUS" != "true" ]; then
-        echo "[$DATE] ❌ $SERVICE est arrêté. Redémarrage..." >> $LOGFILE
+        echo "[$DATE] ❌ $SERVICE arrêté. Tentative de relance..." >> $LOGFILE
         docker start $SERVICE >> $LOGFILE 2>&1
     else
-        echo "[$DATE] ✅ $SERVICE fonctionne correctement." >> $LOGFILE
+        USAGE=$(docker stats $SERVICE --no-stream --format "{{.CPUPerc}}" 2>/dev/null)
+        echo "[$DATE] ✅ $SERVICE en ligne (Charge CPU: $USAGE)" >> $LOGFILE
     fi
 done
 
